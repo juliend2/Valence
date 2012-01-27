009a722324f4085c7db4ddf5091798b0aded95ad
Julien Desrosiers <juliend2@gmail.com>
2012-01-27 15:15:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+Valence
+=======
+
+Valence is a DSL for interacting with a server via SSH.
+
+Installation
+------------
+
+1. Install the SSH2 php library
+  
+(you might need sudo)
+  
+    $ pecl install ssh2 channel://pecl.php.net/ssh2-0.11.3
+2. git clone this repository in your php include_path
+3. add <path-to-valence>/bin to your shell $PATH
+
+Usage
+-----
+
+1. create a valencefile.php file in your project root.
+
+2. in this file, set up the connection like this:
+
+    $current_directory = '/home/remoteuser'; // this is your starting path. must be set before the connect() call
+    connect(array(
+      'host'=>'hostname.com', 
+      'user'=>'remoteuser',
+      'publickeyfile'=> '/home/username/.ssh/id_rsa.pub',
+      'privatekeyfile'=> '/home/username/.ssh/id_rsa'
+    ));
+
+3. you can now use the Valence DSL:
+
+    echo run('ls');
+    cd('/home/remoteuser/src', function() {
+      echo run('ls');
+      run("echo 'joie'");
+    });
+    echo run('ls');
+
+
+
---
+++ valence.php
@@ -0,0 +1,41 @@
+<?php
+
+function connect($options) { // $host, $user, $port=22) {
+  global $connection;
+  $defaults = array(
+    'host' => 'yourhost.com',
+    'user' => 'youruser',
+    'publickeyfile'=> '/home/youruser/.ssh/id_rsa.pub',
+    'privatekeyfile'=> '/home/youruser/.ssh/id_rsa',
+    'port' => 22,
+    'passphrase' => ''
+  );
+  $opts = array_merge($defaults, $options);
+  $connection = ssh2_connect($opts['host'], $opts['port']);
+  // ssh2_auth_password($connection, $user, $pass);
+  if (ssh2_auth_pubkey_file($connection, $opts['user'],
+                            $opts['publickeyfile'],
+                            $opts['privatekeyfile'])) {
+    // echo "Public Key Authentication Successful\n";
+  } else {
+    die('Public Key Authentication Failed');
+  }
+}
+
+function run($command) {
+  global $current_directory, $connection;
+  $stream = ssh2_exec($connection, "cd $current_directory && $command");
+  stream_set_blocking($stream, true);
+  $out = stream_get_contents($stream);
+  fclose($stream);
+  return $out;
+}
+
+function cd($directory, $func) {
+  global $current_directory;
+  $previous_directory = $current_directory;
+  $current_directory = $directory;
+  $func();
+  $current_directory = $previous_directory;
+}
+
