2fb0dfeace4c781fa46979b94e0987359072f7b0
Julien Desrosiers <juliend2@gmail.com>
2012-01-27 21:20:14 -0500

    added local() function

--- valence.php
+++ valence.php
@@ -1,7 +1,18 @@
 <?php
 
+$opts = array();
+
+function _format_command($host, $function, $command) {
+  return "[$host] $function: $command\n";
+}
+
+function _get_remote_host_string() {
+  global $opts;
+  return "{$opts['user']}@{$opts['host']}";
+}
+
 function connect($options) { 
-  global $connection;
+  global $connection, $opts;
   $defaults = array(
     'host' => 'yourhost.com',
     'user' => 'youruser',
@@ -25,11 +36,20 @@ function run($command) {
   stream_set_blocking($stream, true);
   $out = stream_get_contents($stream);
   fclose($stream);
+  print _format_command(_get_remote_host_string(), 'run', $command);
+  return $out;
+}
+
+function local($command) {
+  print _format_command(gethostname(), 'local', $command);
+  exec($command, $out);
+  print implode("\n", $out)."\n";
   return $out;
 }
 
 function cd($directory, $func) {
   global $current_directory;
+  print _format_command(_get_remote_host_string(), 'cd', $directory);
   $previous_directory = $current_directory;
   $current_directory = $directory;
   $func();
